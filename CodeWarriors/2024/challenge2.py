8e2c63cfe7161f58bae8ae436ae96e1863c3fc98
Horvath Zsolt <horvi28@gmail.com>
2024-11-20 09:18:32 +0100

    Have the text of the challenges only in the markdown

--- CodeWarriors/2024/challenge2.py
+++ CodeWarriors/2024/challenge2.py
@@ -1,14 +1,3 @@
-# Greed is a dice game played with give six-sided dice. Your task is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
-# Three 1's => 1000 points
-# Three 6's =>  600 points
-# Three 5's =>  500 points
-# Three 4's =>  400 points
-# Three 3's =>  300 points
-# Three 2's =>  200 points
-# One 1     =>  100 points
-# One 5     =>   50 points
-
-
 triplet_score = {
     1: 1000,
     6: 600,
