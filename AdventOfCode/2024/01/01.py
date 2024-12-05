14d2ea36f4b78d05f43d1df64f232f2927daf7ef
Horvath Zsolt <horvi28@gmail.com>
2024-12-05 13:36:52 +0100

    Add first Advent of code 2024 challenge

---
+++ AdventOfCode/2024/01/01.py
@@ -0,0 +1,16 @@
+with open('./AdventOfCode/2024/01/testinput.txt', 'r') as f:
+    left_list = []
+    right_list = []
+    for line in f:
+        numbers = line.split()
+        left_list.append(int(numbers[0]))
+        right_list.append(int(numbers[1]))
+left_list.sort()
+right_list.sort()
+sub_list = [abs(b - a) for a, b in zip(left_list, right_list)]
+
+print(sum(sub_list)) # Answer for the first question
+
+similarity_list = [l * right_list.count(l) for l in left_list]
+
+print(sum(similarity_list)) # Answer for the second question
