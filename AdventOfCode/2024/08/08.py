c1fd3345a8c85d6a1d97a2347ff28ae0b8027fa3
Horvath Zsolt <horvi28@gmail.com>
2025-01-07 21:05:37 +0100

    Add second half of the 8th puzzle

--- AdventOfCode/2024/08/08.py
+++ AdventOfCode/2024/08/08.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Plan:
+# Plan for the first part:
 # Get all the simbols from the map (that is different than '.')
 # For loop through the symbols
     # Get the coordinates of the specific symbol
@@ -16,18 +16,51 @@ import numpy as np
                 # Mark the antenna as visited (later on we don't need to check this antenna again)
     # Calculate the number of antinodes
 
+# In the first part of the puzzle, antinodes only occures when one of the antennas is twice as far away as the other
+def calculate_antinode_1(antenna1: np.ndarray, antenna2: np.ndarray) -> None:
+    # Calculate the direction vector between the two antenna and calculate the antinodes based on that
+    d = antenna2 - antenna1
 
-def add_antinode(antinode: np.ndarray):
+    antinode1 = antenna1 - d
+    antinode2 = antenna2 + d
+
+    add_antinode(antinode1)
+    add_antinode(antinode2)
+
+
+# In the second part, the distance should not be added only once but until it reaches the end of the map
+def calculate_antinode_2(antenna1: np.ndarray, antenna2: np.ndarray) -> None:
+    # Add the two antenna as antinode
+    add_antinode(antenna1)
+    add_antinode(antenna2)
+
+    # Calculate the other antinodes
+    d = antenna2 - antenna1
+
+    antinode1 = antenna1
+    antinode2 = antenna2
+    while is_antinode_inside_map(antinode1) or is_antinode_inside_map(antinode2):
+        antinode1 = antinode1 - d # -= would make a shallow copy and change antenna1 as well
+        antinode2 = antinode2 + d # Same here
+        add_antinode(antinode1)
+        add_antinode(antinode2)
+
+
+def is_antinode_inside_map(antinode: np.ndarray) -> bool:
+    return antinode[0] < map_size and antinode[1] < map_size and antinode[0] >= 0 and antinode[1] >= 0
+
+
+def add_antinode(antinode: np.ndarray) -> None:
     # Check if it is fitting into the map
-    if antinode[0] < map_size and antinode[1] < map_size and antinode[0] >= 0 and antinode[1] >= 0:
+    if is_antinode_inside_map(antinode):
         # Then add it to the map
         antinodes[tuple(antinode)] = True
+
 
 with open('./AdventOfCode/2024/08/input.txt', 'rt') as f:
     map =  np.array([list(line.strip()) for line in f.readlines()])
     print('Initial map:')
     print(map)
-    print(f'The map has {map.shape} shape')
 
     # Get all the simbols from the map (that is different than '.') and their coordinates
     symbols = set(np.unique(map))
@@ -44,22 +77,17 @@ with open('./AdventOfCode/2024/08/input.txt', 'rt') as f:
 
     for symbol in symbols:
         # So find all the antennas with 'symbol' and make a index tuple list from the positions
-        antenna_positions = np.argwhere(map == symbol)
+        antenna_positions = tuple(np.argwhere(map == symbol))
 
         for antenna1 in antenna_positions:
+            print(f'Check antinodes for antenna {antenna1}')
             visited_antennas[tuple(antenna1)] = True
             for antenna2 in antenna_positions:
                 # Skip the visited ones (This will also skip 'antenna1' as that is already set to visited)
                 if visited_antennas[tuple(antenna2)]:
                     continue
 
-                # Calculate the direction vector between the two antenna
-                d = antenna2 - antenna1
-                antinode1 = antenna1 - d
-                antinode2 = antenna2 + d
-
-                add_antinode(antinode1)
-                add_antinode(antinode2)
+                calculate_antinode_2(antenna1, antenna2)
                 print('Antinode map:')
                 print(antinodes)
     num_antinodes = np.count_nonzero(antinodes)
